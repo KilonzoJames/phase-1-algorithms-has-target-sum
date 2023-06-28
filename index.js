3f1c0d08eaedf20566fe97bdb97be6c84faa33ab
KilonzoJames <jay.kilonzo@gmail.com>
2023-06-28 19:56:03 +0300

    algorithms-has-target-sum

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 function hasTargetSum(array, target) {
+  for(let i=0;i<array.length;i++){
+     // n steps (depending on the length of the input array)
+    const complement=target-array[i];
+    for(j=i+1;j<array.length;j++){
+        // n * n steps (nested loop!)
+      if (array[j]===complement)return true;
+    }
+  }
+  return false;
   // Write your algorithm here
 }
 
